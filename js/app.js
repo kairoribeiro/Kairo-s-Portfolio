38b0f59c3318c41c716912d435822c74c4c67ba9
Kairo Ribeiro <kairo10@gmail.com>
2022-06-10 01:11:26 -0400

    final responsive

--- js/app.js
+++ js/app.js
@@ -1,5 +1,15 @@
 
 
+// Work Buttons
+
+document.getElementById("work1Btn").onclick = function () {
+    location.href = "";
+};
+
 document.getElementById("work2Btn").onclick = function () {
     location.href = "https://kairo1.netlify.app/";
+};
+
+document.getElementById("work3Btn").onclick = function () {
+    location.href = "";
 };

40970a2412abb86169e0f5c27a6ea3f4f2cba408
Kairo Ribeiro <kairo10@gmail.com>
2022-06-14 23:57:42 -0400

    add dark mode

--- README.md
+++ README.md
@@ -1,16 +1,15 @@
 
-Kairo Ribeiro's Web Page
-
-- Sections -
-
-- Contact
-
-- Skills
-
-- Work
+<h1>Kairo Ribeiro's Web Page</h1>
 
 Access here 
+👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇
 
-👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇
+Deployed link: https://prismatic-cuchufli-d013a1.netlify.app/
+Trello planning: https://trello.com/b/j8wRFEy3/kairo-ribeiros-portfolio
 
-https://prismatic-cuchufli-d013a1.netlify.app/
+
+
+
+
+
+
--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 
-
 // Work Buttons
 
 document.getElementById("work1Btn").onclick = function () {
@@ -13,3 +12,28 @@ document.getElementById("work2Btn").onclick = function () {
 document.getElementById("work3Btn").onclick = function () {
     location.href = "";
 };
+
+
+
+
+// Dark mode
+
+const lightDarkBtn = document.querySelector("#light-dark-button")
+const body = document.querySelector("body")
+
+lightDarkBtn.addEventListener("click", toggleLightDark)
+
+function toggleLightDark() {
+    body.className = body.className === "dark" ? "" : "dark"
+  }
+
+function checkDarkPref() {
+    if (
+      window.matchMedia("(prefers-color-scheme:dark)").matches &&
+      body.className !== "dark"
+    ) {
+      toggleLightDark()
+    }
+  }
+
+checkDarkPref()
